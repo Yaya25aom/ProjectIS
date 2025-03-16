657e9fd745c0e7f6c198ce7901a558916f43885f
yanisa <sirinyaausah46@gmail.com>
2025-03-16 22:37:38 +0700

    up

--- pages/page_4.py
+++ pages/page_4.py
@@ -62,7 +62,7 @@ def show():
     }
 
     if run:
-        cap = cv2.VideoCapture(1)
+        cap = cv2.VideoCapture(0)
         stframe = st.empty()
         stop_button = st.button("🔴 ปิดกล้อง")
 
